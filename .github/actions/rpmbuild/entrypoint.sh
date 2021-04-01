6ff14334d85cef7e9032fc1bb8e2a0dcf35873da
Nicolas Trangez <ikke@nicolast.be>
2021-04-01 22:52:55 +0000

    Stash

--- .github/actions/rpmbuild/entrypoint.sh
+++ .github/actions/rpmbuild/entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -xue -o pipefail
 
+install -g build -o build -m 0444 \
+    $GITHUB_WORKSPACE/python-gptsum.spec \
+    /home/build/rpmbuild/SPECS/
+install -g build -o build -m 0444 \
+    $GITHUB_WORKSPACE/dist/gptsum-$1.tar.gz \
+    /home/build/rpmbuild/SOURCES/
+
 su - build -s /bin/bash << EOF
 set -xue -o pipefail
 
@@ -13,7 +20,8 @@ test -f /home/build/rpmbuild/SRPMS/python-gptsum-$1-1.el8.src.rpm
 test -f /home/build/rpmbuild/RPMS/noarch/python3-gptsum-$1-1.el8.noarch.rpm
 EOF
 
-install -g $HOST_GID -o $HOST_UID -m 0444 \
+mkdir $GITHUB_WORKSPACE/artifacts/
+install -m 0444 \
     /home/build/rpmbuild/SRPMS/python-gptsum-$1-1.el8.src.rpm \
     /home/build/rpmbuild/RPMS/noarch/python3-gptsum-$1-1.el8.noarch.rpm \
-    /artifacts/
+    $GITHUB_WORKSPACE/artifacts/
